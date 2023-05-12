f907f00c45d86e8c24ce9e7b7c44bf71055a8daf
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-05-12 10:31:25 +0200

    removed the delete post request

--- services/posts/src/posts.service.ts
+++ services/posts/src/posts.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PostEntity } from './posts.entity';
-import { CreatePostRequest, DeletePostRequest } from './posts.request';
+import { CreatePostRequest } from './posts.request';
 
 @Injectable()
 export class PostsService {
   public constructor(@InjectRepository(PostEntity) private readonly postRepository: Repository<PostEntity>) { }
 
-  public getPosts() {
-    return this.postRepository.find();
+  public async getPosts() {
+    const posts = await this.postRepository.find();
+
+    return posts;
   }
 
   public createPost(createPostRequest: CreatePostRequest) {
@@ -18,7 +20,7 @@ export class PostsService {
     return this.postRepository.save(post);
   }
 
-  public deletePost(deletePostRequest: DeletePostRequest) {
-    return this.postRepository.delete(deletePostRequest.id);
+  public deletePost(id: string) {
+    return this.postRepository.delete({ id });
   }
 }
