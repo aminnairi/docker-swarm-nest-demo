22765c943c657aaf885d27859edaa990716e53d1
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-05-12 10:29:15 +0200

    turned the client proxy into an http request

--- services/gateway/src/posts/posts.controller.ts
+++ services/gateway/src/posts/posts.controller.ts
@@ -1,22 +1,24 @@
-import { Body, Controller, Delete, Get, Inject, Param, Post } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Body, Controller, Get, Param, Delete, Post, UseFilters } from '@nestjs/common';
+import { PostsExceptionFilter } from './posts.filter';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
+@UseFilters(PostsExceptionFilter)
 export class PostsController {
-  public constructor(@Inject("POSTS_SERVICE") private readonly postsProxy: ClientProxy) { }
+  public constructor(public readonly postsService: PostsService) { }
 
   @Get()
-  public async getPosts() {
-    return this.postsProxy.send("getPosts", {});
+  public getPosts() {
+    return this.postsService.getPosts();
   }
 
   @Post()
   public createPost(@Body() body: unknown) {
-    return this.postsProxy.send("createPost", body);
+    return this.postsService.createPost(body);
   }
 
   @Delete(":id")
-  public deletePost(@Param("id") id: string) {
-    return this.postsProxy.send("deletePost", { id });
+  public deletePost(@Param("id") id: string, body: unknown) {
+    return this.postsService.deletePost(id, body);
   }
 }
