e7c3e2de6afa9ecaa2b4d71333bfcebbcfbc69f4
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-05-12 10:28:47 +0200

    added the http module & posts module

--- services/gateway/src/app.module.ts
+++ services/gateway/src/app.module.ts
@@ -1,7 +1,10 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
+import { PostsService } from "./posts/posts.service";
 
 @Module({
-  imports: [PostsModule],
+  imports: [HttpModule, PostsModule],
+  providers: [PostsService]
 })
 export class AppModule { }
