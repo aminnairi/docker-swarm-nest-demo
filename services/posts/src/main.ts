03e6be447dfa56df66d39aece3c2a17ebc8eba80
aminnairi <18418459+aminnairi@users.noreply.github.com>
2023-05-12 10:29:59 +0200

    converted to an http microservice

--- services/posts/src/main.ts
+++ services/posts/src/main.ts
@@ -1,16 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { PostsModule } from './posts.module';
-import { Transport } from "@nestjs/microservices";
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(PostsModule, {
-    transport: Transport.TCP,
-    options: {
-      host: "0.0.0.0",
-      port: 3001
-    }
-  });
-
-  await app.listen();
+  const app = await NestFactory.create(PostsModule);
+  await app.listen(80);
 }
+
 bootstrap();
